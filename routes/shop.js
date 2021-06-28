76aaba5fbf88a152b2094d324b97a1072993a3e8
Pratik Kshirsagar <meetpratiik1995@gmail.com>
2021-06-29 00:10:55 +0530

    pug templating added

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,8 @@ const adminData = require('./admin');
 const routes = express.Router();
 
 routes.get('/', (req, res, next) => {
-  console.log('shopJS', adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
 });
 
 module.exports = routes;
