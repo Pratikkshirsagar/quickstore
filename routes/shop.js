512b0b472f14f0111074d54a91434fe79597fd51
Pratik Kshirsagar <meetpratiik1995@gmail.com>
2021-06-28 23:41:46 +0530

    template added

--- routes/shop.js
+++ routes/shop.js
@@ -3,10 +3,12 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const routes = express.Router();
 
 routes.get('/', (req, res, next) => {
+  console.log('shopJS', adminData.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
